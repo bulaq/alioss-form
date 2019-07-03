ef0836cd9a41b38cd5248e6d8a26b05333a1b373
xirwajim <xirwajim@gmail.com>
2019-07-03 13:41:16 +0800

    process

--- resources/views/image.blade.php
+++ resources/views/image.blade.php
@@ -6,13 +6,14 @@
         @if(($column_val = old($column, $value)))
             <?php
                 $oss_url = config('alioss')['OSS_URL'];
+                $oss_process = config('alioss')['OSS_PROCESS'];
                 $column_val = is_array($column_val) ? $column_val : explode(',', $column_val);
             ?>
             <div class="upload_warp" id="{{$column}}_upload_warp" style="opacity: 1; display: block;">
                 @foreach($column_val as $p)
                 <div class="upload_item">
                     <span class="upload_del_btn" data-filename="{{$p}}" onclick="del_pic(this,true,'undefined')">删除</span>
-                    <img src="{{$oss_url}}/{{$p}}?x-oss-process=image/resize,m_fill,w_100,h_100">
+                    <img src="{{$oss_url}}/{{$p}}{{$oss_process}}">
                     <input type="hidden" class="Js_upload_input" name="{{$column}}[]" value="{{$p}}">
                 </div>
                 @endforeach
